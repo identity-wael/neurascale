b107267acafb2c21798283f8dca595947f1d6d41
identity-wael <identity@wael.ai>
2025-07-02 15:15:31 -0400

    Add attractors example page

---
+++ app/attractors/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function AttractorsPage() {
+  return (
+    <div className="w-screen h-screen">
+      <iframe src="/attractors.html" className="w-full h-full border-0" />
+    </div>
+  )
+}
